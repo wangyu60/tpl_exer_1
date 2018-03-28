a9c86df3849dcbe90e945388d8acc98862ec47dd
Wang Yu <wangyu60@foxmail.com>
2018-03-28 15:17:03 +0800

    try adding a new class file

---
+++ tpl_exer_1/NewClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpl_exer_1
+{
+    public class NewClass
+    {
+    }
+}

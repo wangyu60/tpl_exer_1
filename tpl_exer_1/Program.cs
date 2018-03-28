713bab416050516a5d47e213d69d2eaeceec6179
Wang Yu <wangyu60@foxmail.com>
2018-03-28 13:48:46 +0800

    Add project files.

---
+++ tpl_exer_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpl_exer_1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello, world!");
+            Console.Read();
+        }
+    }
+}
